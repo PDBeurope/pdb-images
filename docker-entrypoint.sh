03aa5b1fe315b5876ecadbf47ceeb8cf80a97630
Adam Midlik <midlik@gmail.com>
2023-06-08 17:18:15 +0100

    A few logs from docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
-XVFB_FILE=$XVFB_DIR/$$
+export ARGUMENTS=$@
+export XVFB_FILE=$XVFB_DIR/$$
 echo "XVFB_FILE=$XVFB_FILE" 1>&2
-xvfb-run --auto-servernum -f $XVFB_FILE node /pdbe-images/build/index.js $@
+test -d "$XVFB_DIR" || echo "Failed because specified XVFB_DIR does not exist: $XVFB_DIR" 1>&2
+xvfb-run --auto-servernum -f "$XVFB_FILE" bash -c 'echo DISPLAY=$DISPLAY 1>&2; test -e $XVFB_FILE && echo "Xvfb file exists: $XVFB_FILE" 1>&2 || echo "Xvfb file does not exist: $XVFB_FILE" 1>&2; node /pdbe-images/build/index.js $ARGUMENTS'
 STATUS=$?
-rm -f $XVFB_FILE
+rm -f $XVFB_FILE || echo "Failed to remove Xvfb file $XVFB_FILE in dir $XVFB_DIR" 1>&2
 exit $STATUS
--- src/captions/collect.ts
+++ src/captions/collect.ts
@@ -37,8 +37,8 @@ export function collectCaptions(directory: string, pdbId: string, lastModificati
     filenameStems.sort();
     result['image_suffix'] = getCommonSuffixes(allFiles, filenameStems);
     result['last_modification'] = lastModificationDate ?? new Date().toISOString().split(/[T ]/)[0]; // format as 2023-03-21
-    fs.writeFileSync(path.join(directory, `${pdbId}.json`), JSON.stringify({ [pdbId]: result }, undefined, 2));
-    fs.writeFileSync(path.join(directory, `${pdbId}_filelist`), filenameStems.join('\n'));
+    fs.writeFileSync(path.join(directory, `${pdbId}.json`), JSON.stringify({ [pdbId]: result }, undefined, 2) + '\n');
+    fs.writeFileSync(path.join(directory, `${pdbId}_filelist`), filenameStems.join('\n') + '\n');
 }
 
 /** Get the specified section in a nested object `obj`
